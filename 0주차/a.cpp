b7bc8b5934da12ac72578a900b1d2d1a9a2ea0ff
여예원 <98465383+YewonYeo@users.noreply.github.com>
2023-04-04 21:11:44 +0900

    ~1.4

---
+++ 0주차/a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h> // C++의 표준 라이브러리가 포함된 헤더파일
+using namespace std; // std라는 네임스페이스를 사용
+
+int main(){
+  char a = 'a';
+  cout << (int)a  << "\n";
+  cout << (int)a - 97 << "\n";
+  cout << (int)a - 'a' << "\n";
+  
+  
+  return 0;
+}
---
+++ 0주차/a.md
@@ -0,0 +1,3 @@
+g++ -std=c++14 -Wall a.cpp -o test.out;
+
+-> a.cpp 라는 파일을 엄격하게(-wall) c++14 버전으로 컴파일해서 test.out라는 파일을 만든다
